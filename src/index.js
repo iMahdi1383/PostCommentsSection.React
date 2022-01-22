b9fc133cac2d0cebddd52d49af97ec72e04e4495
Mahdi <mahdiaskarpoor1383@gmail.com>
2022-01-22 15:47:05 +0330

    Edit 'src/index.js' : Add random fake image url for comment avatar.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import faker from "faker";       // its not working
 
 const App = () => {
     return (
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    {/* use this instead of 'faker.image.avatar()' */}
+                    <img alt="avatar" src="https://i.pravatar.cc/150" />
                 </a>
                 <div className="content">
                     <a href="/" className="author">
