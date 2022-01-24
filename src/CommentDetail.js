bcc2a262282a87a910767e85f249fa0498bb16e4
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-01-24 12:28:33 +0330

    Edit 'src/index.js' : Add 'props.author'

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                {/* use this instead of 'faker.image.avatar()' */}
-                {/* <img alt="avatar"  src="https://source.unsplash.com/random/50x50/?avatar" /> */}
                 <img alt="avatar" src="https://i.pravatar.cc/50" />
             </a>
             <div className="content">
                 <a href="/" className="author">
-                    Mahdi
+                    {props.author}
                 </a>
                 <div className="metadata">
                     <span className="date">Today at 6:00PM</span>
