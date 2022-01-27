e1516126b2392aaea074ce95285fd23989b73472
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-01-27 17:19:08 +0330

    Edit 'src/index.js','src/CommentDetail.js' : Add more props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -4,16 +4,16 @@ const CommentDetail = (props) => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src="https://i.pravatar.cc/50" />
+                <img alt="avatar" src={props.image} />
             </a>
             <div className="content">
                 <a href="/" className="author">
                     {props.author}
                 </a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
+                    <span className="date">{props.timeAgo}</span>
                 </div>
-                <div className="text">Nice blog post!</div>
+                <div className="text">{props.content}</div>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="ALex" />
-            <CommentDetail author="Jane" />
+            <CommentDetail
+                author="Sam"
+                timeAgo="2 minutes ago"
+                content="Nice blog post!"
+                image="https://i.pravatar.cc/50"
+            />
+            <CommentDetail
+                author="ALex"
+                timeAgo="Today, 14:45"
+                content="This is some example text for a comment."
+                image="https://i.pravatar.cc/45"
+            />
+            <CommentDetail
+                author="Jane"
+                timeAgo="Yesterday, 21:00"
+                content="Hello World!"
+                image="https://i.pravatar.cc/55"
+            />
         </div>
     );
 };
