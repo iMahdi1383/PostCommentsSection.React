cc74558cbb45d4ae58115c54a7f02f5a19334b6a
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-01-28 15:23:26 +0330

    Create 'src/ApprovalCard.js'  |   then import it to 'src/index.js'

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import faker from "faker";       // its not working
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard />
             <CommentDetail
                 author="Sam"
                 timeAgo="2 minutes ago"
