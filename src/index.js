d39e1acd819833506818b3ee3f2fb91ac151285e
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-01-28 15:39:55 +0330

    Edit 'index.js','ApprovalCard.js' : Add 'CommentDetail' As a children to 'ApprovalCard'

--- src/index.js
+++ src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail
-                author="Sam"
-                timeAgo="2 minutes ago"
-                content="Nice blog post!"
-                image="https://i.pravatar.cc/50"
-            />
-            <CommentDetail
-                author="ALex"
-                timeAgo="Today, 14:45"
-                content="This is some example text for a comment."
-                image="https://i.pravatar.cc/45"
-            />
-            <CommentDetail
-                author="Jane"
-                timeAgo="Yesterday, 21:00"
-                content="Hello World!"
-                image="https://i.pravatar.cc/55"
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="2 minutes ago"
+                    content="Nice blog post!"
+                    image="https://i.pravatar.cc/50"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="ALex"
+                    timeAgo="Today, 14:45"
+                    content="This is some example text for a comment."
+                    image="https://i.pravatar.cc/45"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Yesterday, 21:00"
+                    content="Hello World!"
+                    image="https://i.pravatar.cc/55"
+                />
+            </ApprovalCard>
         </div>
     );
 };
